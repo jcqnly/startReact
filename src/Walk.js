8620192e8588341c7813bbdecfcd6112de7c87d4
jackie <jcqnly@gmail.com>
2019-01-19 15:11:25 -0800

    move components into separate files

---
+++ src/Walk.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Walk()
+{
+  return(
+    <div>
+      <h1>Hi, Fred</h1>
+      <p>It's a beautiful day in the neighborhood.  Let's:</p>
+      <ul>
+        <li>stroll though the park</li>
+        <li>have a picnic</li>
+        <li>and people watch</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Walk
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import Walk from "./Walk"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ul><li>bread</li><li>veggies</li><li>fruit</li></ul>, document.getElementById('root'));
 
+ReactDOM.render(
+  <Walk />,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister();
